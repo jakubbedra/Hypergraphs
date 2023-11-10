766ecf822bd78e25e39c4401085e0110d19dd26c
theKonfyrm <jakubbedra@wp.pl>
2023-11-10 10:16:44 +0100

    .

---
+++ Hypergraphs/Graphs/Generators/TreeGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Hypergraphs.Graphs.Model;
+
+namespace Hypergraphs.Graphs.Generators;
+
+public class TreeGenerator
+{
+    private readonly Random _r;
+
+    public TreeGenerator()
+    {
+        _r = new Random();
+    }
+    
+    public Graph Generate(int n)
+    {
+        Graph g = new Graph(n);
+        for (int v = 1; v < n; v++)
+        {
+            int u = _r.Next(v);
+            g.AddEdge(v, u);
+        }
+
+        return g;
+    }
+    
+}
--- Hypergraphs/Hypergraphs/Generators/HypertreeGenerator.cs
+++ Hypergraphs/Hypergraphs/Generators/HypertreeGenerator.cs
@@ -1,6 +1,46 @@
-﻿namespace Hypergraphs.Generators;
+﻿using Hypergraphs.Graphs.Model;
+using Hypergraphs.Model;
+
+namespace Hypergraphs.Generators;
 
 public class HypertreeGenerator
 {
+    private static readonly Random _r = new Random();
+    // todo: do not support m?
+    public Hypergraph Generate(int n, int m, int bound = -1)
+    {
+        bool[] selected = new bool[n];
+        for (int i = 0; i < n; i++)
+            selected[i] = false;
+
+        if (bound == -1)
+            bound = n;
+
+        if (bound > n)
+        {
+            throw new ArgumentException($"Maximum edge cardinality should not be greater than n, but was {bound} and n was {n}.", "bound");
+        }
+        
+        // 1. wygeneruj drzewo
+        Graph t = ...;
+// todo: bound to maks wierzcholkow w krawedzi
+        while (selected.Contains(false))
+        {
+            // 2. wybierz losowo wierzcholek
+            
+            
+            // 3. wybierz losowo liczbe od 1 do bounded (tyle wierzcholkow bedzie w hiperkrawedzi)
+
+            
+            // mozemy wybierac sasiadow i zostawiac 1 z nich na 100% a kazdego kolejnego z coraz mniejszym prawdopodobienstwem
+            
+        }
+
+        // 4. wyznacz spojny podgraf poczawszy od tego wierzcholka, ktory bedzie mial dlugosc m
+        // (random walk?)
+        // 5. podgraf staje sie krawedzia
+        // 6. powtarzaj 1-5 dopoki istnieja wierzcholki izolowane
+        return null;
+    }  
     
 }
