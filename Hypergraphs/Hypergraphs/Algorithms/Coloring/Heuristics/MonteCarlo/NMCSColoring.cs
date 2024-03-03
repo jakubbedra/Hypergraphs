211283e9d97f5c585167d2b7f80498cb1099f8b3
jakubbedra <jakubbedra@wp.pl>
2024-03-03 21:31:00 +0100

    nigga

--- Hypergraphs/Hypergraphs/Algorithms/Coloring/Heuristics/MonteCarlo/NMCSColoring.cs
+++ Hypergraphs/Hypergraphs/Algorithms/Coloring/Heuristics/MonteCarlo/NMCSColoring.cs
@@ -1,6 +1,29 @@
-﻿namespace Hypergraphs.Algorithms;
+﻿using Hypergraphs.Common.Algorithms;
+using Hypergraphs.Extensions;
+using Hypergraphs.Model;
 
-public class NMCSColoring
+namespace Hypergraphs.Algorithms;
+
+public class NMCSColoring : BaseColoring<Hypergraph>
 {
+    private readonly int NumberOfEpochs = 10;
+    private readonly int MaxDepth = 10;
+    
+    public override int[] ComputeColoring(Hypergraph hypergraph)
+    {
+        List<int> vertices = new List<int>();
+        for (int v = 0; v < hypergraph.N; v++)
+            vertices.Add(0);
+        vertices.Shuffle();
+
+        for (int c = 2; c < hypergraph.N; c++)
+        {
+            NMCS nrpa = new NMCS(hypergraph, c, NumberOfEpochs, MaxDepth, vertices.ToArray());
+            int[]? colors = nrpa.ComputeColoring();
+            if (colors != null) return colors;
+        }
+
+        throw new Exception();
+    }
     
 }
--- Hypergraphs/Hypergraphs/Algorithms/Coloring/Heuristics/MonteCarlo/NRPAColoring.cs
+++ Hypergraphs/Hypergraphs/Algorithms/Coloring/Heuristics/MonteCarlo/NRPAColoring.cs
@@ -1,12 +1,29 @@
 ﻿using Hypergraphs.Common.Algorithms;
+using Hypergraphs.Extensions;
 using Hypergraphs.Model;
 
 namespace Hypergraphs.Algorithms;
 
 public class NRPAColoring : BaseColoring<Hypergraph>
 {
+    private readonly int NumberOfEpochs = 10;
+    private readonly int MaxDepth = 10;
+    
     public override int[] ComputeColoring(Hypergraph hypergraph)
     {
-        NRPA nrpa = new NRPA(hypergraph, 2, 10, );
+        List<int> vertices = new List<int>();
+        for (int v = 0; v < hypergraph.N; v++)
+            vertices.Add(0);
+        vertices.Shuffle();
+
+        for (int c = 2; c < hypergraph.N; c++)
+        {
+            // todo: vertex order?
+            NRPA nrpa = new NRPA(hypergraph, c, NumberOfEpochs, MaxDepth, vertices.ToArray());
+            int[]? colors = nrpa.ComputeColoring();
+            if (colors != null) return colors;
+        }
+
+        throw new Exception();
     }
 }
